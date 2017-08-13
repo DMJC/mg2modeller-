d2873650dca59646f956e07987c5ba1cfe8b506e
James Carthew <james@James-Work>
2017-08-13 23:45:40 +1000

    Initial import of Major TrueSpace style User Interface.

---
+++ include/gui/mg2toolbutton.h
@@ -0,0 +1,46 @@
+#ifndef MG2TOOLBUTTON_H
+#define MG2TOOLBUTTON_H
+#include <gtkmm.h>
+//#include "tool.h"
+#include <iostream>
+#include <string>
+
+struct tool{
+    string name;
+    string tooltip;
+    string image_filename;
+    callback left_click_callback;
+    callback right_click_callback;
+    callback long_left_click_callback;
+    callback long_right_click_callback;
+};
+
+struct pressrelease {
+    unsigned int button;
+    unsigned int time;
+};
+
+class ToolButton : public Gtk::Button
+{
+protected:
+    Gtk::Image *buttonimage;
+
+    std::list<tool> *tool_list;
+    std::string name;
+    std::string tooltip;
+    std::string image_filename;
+    callback left_click_callback;
+    callback right_click_callback;
+    callback long_left_click_callback;
+    callback long_right_click_callback;
+    int start_time;
+public:
+    ToolButton(std::list<tool>& tool_list);
+    void set_button(std::list<tool>& tool_list, int position);
+    unsigned int mg2_button_press(GdkEventButton * event);
+    unsigned int mg2_button_release(GdkEventButton * event);
+    int Trigger_Menu(std::list<tool>& tool_list, GdkEventButton * event);
+    void Short_Left_Click(void);
+    void Short_Right_Click(void);
+};
+#endif //MG2TOOLBUTTON_H
---
+++ src/gui/mg2toolbutton.cpp
@@ -0,0 +1,104 @@
+#include "mg2toolbutton.h"
+
+ToolButton::ToolButton(std::list<tool>& tool_list)
+{
+    std::cout << "Initialised" << std::endl;
+    this->start_time = 0;
+    this->signal_button_press_event().connect(sigc::mem_fun(this, &ToolButton::mg2_button_press), false);
+    this->signal_button_release_event().connect(sigc::mem_fun(this, &ToolButton::mg2_button_release), false);
+    this->name = tool_list.front().name;
+    this->tooltip = tool_list.front().tooltip;
+    this->image_filename = tool_list.front().image_filename;
+    void (*left_click_callback)(void) = tool_list.front().left_click_callback;
+    void (*right_click_callback)(void) = tool_list.front().right_click_callback;
+    void (*long_left_click_callback)(void) = tool_list.front().long_left_click_callback;
+    void (*long_right_click_callback)(void) = tool_list.front().long_right_click_callback;
+    this->tool_list = &tool_list;
+    this->set_size_request(32, 24);
+    this->buttonimage = new Gtk::Image( this->image_filename );
+    this->set_image(*this->buttonimage);
+    this->show();
+}
+
+void ToolButton::set_button(std::list<tool>& tool_list, int position){
+    std::list<tool>::iterator it = tool_list.begin();
+    std::advance(it, position);
+    std::cout << "moving item: " << position << " to front of the list." << std::endl;
+    this->image_filename = it->image_filename;
+    this->buttonimage = new Gtk::Image( this->image_filename );
+    this->set_image(*this->buttonimage);
+    this->left_click_callback = it->left_click_callback;
+    this->right_click_callback = it->right_click_callback;
+    this->long_left_click_callback = it->long_left_click_callback;
+    this->long_right_click_callback = it->long_right_click_callback;
+};
+
+
+unsigned int ToolButton::mg2_button_press(GdkEventButton * event){
+    std::cout << "Button Pressed at: " << event->time << std::endl;
+    this->start_time = event->time;
+}
+
+unsigned int ToolButton::mg2_button_release(GdkEventButton * event){
+    std::cout << "Button Released at: " << event->time << std::endl;
+    unsigned int finish_time = 0;
+    finish_time = event->time;
+	std::cout << "Finish Time: " << finish_time << std::endl;
+
+	switch (event->button){
+
+	case 3:
+	    if ((finish_time - this->start_time) >= 200){
+		    std::cout << "Menu click" << std::endl;
+			this->Trigger_Menu(*this->tool_list, event);
+	    } else {
+		    std::cout << "short right click" << std::endl;
+			if(this->right_click_callback != nullptr){
+			    this->right_click_callback();
+			}else{
+			    std::cout << "No Right Click Action" << std::endl;
+			}
+	    }
+	break;
+	default:
+	    if ((finish_time - this->start_time) >= 200){
+		    std::cout << "Menu click" << std::endl;
+			this->Trigger_Menu(*this->tool_list, event);
+	    } else {
+		    std::cout << "short left click" << std::endl;
+			if(this->left_click_callback != nullptr){
+			    this->left_click_callback();
+			}else{
+			    std::cout << "No Left Click Action" << std::endl;
+			}
+	    }
+	}
+	std::cout << "Click Took: " << finish_time - this->start_time << std::endl;
+}
+
+int ToolButton::Trigger_Menu(std::list<tool>& tool_list, GdkEventButton * event){
+    std::cout << "Opening Menu" << std::endl;
+    int list_size = 0;
+    list_size = tool_list.size();
+    list<string>::const_iterator current_tool;
+    std::cout << "list is: " << list_size << " elements long" <<std::endl;
+    Gtk::Menu *Menu;
+    Menu = Gtk::manage(new Gtk::Menu);
+    int i = 0;
+    for(list<tool>::const_iterator current_tool = tool_list.begin(); current_tool != tool_list.end(); current_tool++){
+        std::cout << (*current_tool).name << std::endl;
+    	Gtk::Image *ButtonImage = Gtk::manage(new Gtk::Image((*current_tool).image_filename));
+        Gtk::MenuItem *MenuItem = Gtk::manage(new Gtk::MenuItem(*ButtonImage));
+	MenuItem->set_size_request(32, 24);
+        MenuItem->signal_activate().connect(sigc::bind(sigc::mem_fun(*this, &ToolButton::set_button), tool_list, i));
+        std::cout << (*current_tool).tooltip << std::endl;
+    	std::cout << (*current_tool).image_filename << std::endl;
+    	Menu->attach(*MenuItem,0,1,i,i+1);
+        i++;
+    }
+    Menu->set_size_request(32, 24);
+    Menu->set_reserve_toggle_size(FALSE);
+    Menu->popup_at_widget(this, Gdk::GRAVITY_NORTH, Gdk::GRAVITY_SOUTH, nullptr);
+    Menu->show_all();
+    std::cout << "Showing Tools" << std::endl;
+}
